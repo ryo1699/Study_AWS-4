fa5d90cce771cf1c797fba2ca931552f9f1c01a2
ryo-ddkjovi <ryo20040604@gmail.com>
2026-05-23 17:24:39 +0900

    課題2push

--- Study_AWS-4_1/infra/terraform/main.tf
+++ Study_AWS-4_1/infra/terraform/main.tf
@@ -180,6 +180,7 @@ resource "aws_security_group" "bastion" {
 
 resource "aws_ecr_repository" "api" {
   name                 = "${local.name}-api"
+  force_delete         = true
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
